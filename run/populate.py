5f601468f02044c50a7e3f945c9363b172979487
justinyates887 <justinyates887@gmail.com>
2021-04-16 10:32:03 -0500

    Added more logic!

--- run/populate.py
+++ run/populate.py
@@ -32,9 +32,8 @@ def add_github(entry, preview_entry):
         'repo': github_info[4]
     }
 
-def add_markdown(entry_data, entry, pe):
-    return
-
+#Buttons
+#############################################################################
 def add_license(preview_entry):
     if not githubURL:
         error_window()
@@ -44,8 +43,64 @@ def add_license(preview_entry):
     path = 'github/license/' + user_repo['user'] + '/' + user_repo['repo']
     pe.insert(END, '![license](' + shield + path + ')\n')
 
+def add_forks(preview_entry):
+    if not githubURL:
+        error_window()
 
-def get_data(entry, preview_entry):
+    pe = preview_entry
+    shield = 'https://img.shields.io/'
+    path = 'github/forks/' + user_repo['user'] + '/' + user_repo['repo']
+    pe.insert(END, '![forks](' + shield + path + ')\n')
+
+def add_stars(preview_entry):
+    if not githubURL:
+        error_window()
+
+    pe = preview_entry
+    shield = 'https://img.shields.io/'
+    path = 'github/stars/' + user_repo['user'] + '/' + user_repo['repo']
+    pe.insert(END, '![stars](' + shield + path + ')\n')
+
+def add_size(preview_entry):
+    if not githubURL:
+        error_window()
+
+    pe = preview_entry
+    shield = 'https://img.shields.io/'
+    path = 'github/languages/code-size/' + user_repo['user'] + '/' + user_repo['repo']
+    pe.insert(END, '![size](' + shield + path + ')\n')
+
+def add_lines(preview_entry):
+    if not githubURL:
+        error_window()
+
+    pe = preview_entry
+    shield = 'https://img.shields.io/'
+    path = 'tokei/lines/github/' + user_repo['user'] + '/' + user_repo['repo']
+    pe.insert(END, '![lines](' + shield + path + ')\n')
+
+def add_pulls(preview_entry):
+    if not githubURL:
+        error_window()
+
+    pe = preview_entry
+    shield = 'https://img.shields.io/'
+    path = 'github/issues-pr-closed/' + user_repo['user'] + '/' + user_repo['repo']
+    pe.insert(END, '![pulls](' + shield + path + ')\n')
+
+#############################################################################
+
+def add_markdown(entry_data, heading, pe):
+    pe.insert(END, '\n\n##' + heading + '\n\n' + entry_data)
+
+def get_text_data(type, entry, preview_entry):
+    heading = type
+    pe = preview_entry
+    entry_data = entry.get("1.0",'end-1c')
+    add_markdown(entry_data, heading, pe)
+
+def get_entry_data(type, entry, preview_entry):
+    heading = type
     pe = preview_entry
     entry_data = entry.get()
-    add_markdown(entry_data, entry, pe)
+    add_markdown(entry_data, heading, pe)
